1e79a802e6d0a5b9723f1ebcc7fb1363bb7fe780
sekiguchi gaku <s214660x@st.go.tuat.ac.jp>
2023-01-23 20:07:21 +0900

    configの整理

--- core/logger.py
+++ core/logger.py
@@ -63,15 +63,15 @@ def parse(args):
 
     # debug
     if 'debug' in opt['name']:
-        opt['train']['val_freq'] = 100
-        opt['train']['over_val'] = 10000
-        opt['train']['over_train_print'] = 10000
+        opt['train']['val_freq'] = 50
+        opt['train']['over_val'] = 0
+        opt['train']['over_train_print'] = 0
         opt['train']['print_freq'] = 50
         opt['train']['train_print_freq'] = 50
         opt['datasets']['train']['batch_size'] = 8
-        opt['model']['beta_schedule']['train']['n_timestep'] = 2000
+        opt['model']['beta_schedule']['train']['n_timestep'] = 2
         opt['model']['beta_schedule']['val']['n_timestep'] = 2
-        opt['datasets']['train']['data_length'] = 200
+        opt['datasets']['train']['data_length'] = 8
         opt['datasets']['val']['data_length'] = -1
 
     # W&B Logging
